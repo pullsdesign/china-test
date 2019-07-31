4021c747f5d089f5fa111c00852c8a077f80dcc6
Alex <alexlxela94@gmail.com>
2019-08-01 01:07:24 +0300

    Added couple interfaces, add test component style, add start page for tests

---
+++ src/containers/Test/StartPage/StartPage.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './StartPage.scss';
+import {Props} from './interfaces';
+
+import Lighter from '../../../assets/imgs/lighter-icon.png';
+import LeftArrowIcon from '../../../assets/imgs/left-arrow.png'
+
+function StartPage(props: Props) {
+  return (
+    <div className="start-page">
+      <p className="start-page__title">Welcome to ChinesePod placement test</p>
+      <p className="start-page__subtitle">If you are a learner of traditional characters, please select the
+        option below and go to the test. Otherwise, the test will be in simplified format.</p>
+      <label className="start-page__option">
+        <img src={Lighter} alt="Lighter icon"/>
+        <p>I want to use<br/>TRADITIONAL characters</p>
+        <input type="checkbox"/>
+        <span className="start-page__checkbox-immit"/>
+      </label>
+      <button
+        className="start-page__btn"
+        onClick={() => props.onStartTest()}
+      >Go to the test<img src={LeftArrowIcon} alt="Arrow"/></button>
+    </div>
+  )
+}
+
+export default StartPage;
--- src/containers/Test/Test.tsx
+++ src/containers/Test/Test.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import * as React from 'react';
+import './Test.scss'
+import {State} from './interfaces';
 
-export default class Test extends React.Component {
-    render() {
-        return (
-            <div>
-                Test comp
-            </div>
-        )
-    }
+import StartPage from './StartPage/StartPage';
+
+export default class Test extends React.Component<Object, State> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      testStarted: false
+    };
+
+    this.startTest = this.startTest.bind(this);
+  }
+
+  startTest() {
+    this.setState({
+      testStarted: true
+    })
+  }
+
+  render() {
+    return (
+      <div className="test container">
+        {
+          this.state.testStarted ? <p>test started</p> : <StartPage onStartTest={this.startTest}/>
+        }
+      </div>
+    )
+  }
 }
